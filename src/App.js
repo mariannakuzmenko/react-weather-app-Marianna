f12fa849dc38555fda43aeed9301cbe9174166d0
mariannakuzmenko <111392697+mariannakuzmenko@users.noreply.github.com>
2023-08-08 01:17:00 +0200

    added the GitHub uhl

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a href="https://mariannaportfolio.netlify.app/" target="_blank">
+        Marianna Kuzmenko
+        </a>{""}
+        and is{""}
+        <a href="https://github.com/mariannakuzmenko/react-weather-app-Marianna" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
